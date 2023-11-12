15a8a814ef23d58af1282b859230e83cbb4fc5bf
amirmahdifalahi <amirfalahi6868@gmail.com>
2023-11-12 20:52:18 +0330

    I think this is the final commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // is prime function
 const isPrime = function (x) {
   let temp = true;
@@ -16,7 +18,7 @@ const calc = function (x) {
   let primeNumbers = "";
   let primeCounter = 0;
   let lastPrimeNumber;
-  for (let i = 1; i <= x; i++) {
+  for (let i = 2; i <= x; i++) {
     if (isPrime(i)) {
       primeNumbers += `${i} -`;
       primeCounter += 1;
@@ -31,6 +33,32 @@ const calc = function (x) {
 
 // calc(100)
 
-const isPrimeInput = document.querySelector("is-prime-input");
+const isPrimeInput = document.querySelector(".is-prime-input");
+const isPrimeResult = document.querySelector(".is-prime-result");
+const btn = document.querySelector(".btn");
+const primeCounterValue = document.querySelector(".prime-counter-value");
+const lastPrimeNumber = document.querySelector(".last-prime-number");
 
-isPrimeInput.addEventListener("click", function () {});
+const updateIsPrimeInput = function (e) {
+  // console.log(e.target.value);
+  if (!e.target.value) {
+    isPrimeResult.textContent = "";
+  } else {
+    isPrimeResult.textContent = isPrime(Number(e.target.value));
+  }
+};
+
+const updateBtn = function () {
+  const primeCountInput = Number(
+    document.querySelector(".prime-count-input").value
+  );
+  const [PC, LP] = calc(primeCountInput);
+  // console.log(PC);
+  // console.log(LP);
+  primeCounterValue.textContent = `Number of prime numbers: ${PC}`;
+  lastPrimeNumber.textContent = `Last prime number: ${LP}`;
+};
+
+isPrimeInput.addEventListener("input", updateIsPrimeInput);
+
+btn.addEventListener("click", updateBtn);
